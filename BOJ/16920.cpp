d9c3f1183fcbfda084446acf38fae3d7825a0b69
박서진 <kevin8193@naver.com>
2025-05-03 21:19:01 +0900

    2025/05/03 - 09:18PM

--- BOJ/16920.cpp
+++ BOJ/16920.cpp
@@ -1,64 +1,58 @@
 #include <bits/stdc++.h>
 
 int N, M, P;
+
 std::string board[1001];
 int vis[1001][1001];
-int power[10];
+int playerCastle[10];
+int playerPower[10];
 
-int castle[10];
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
 
-std::queue<std::pair<int, int>> q;
+std::vector<std::queue<std::pair<int, int>>> playerQueues;
 
 void Input() {
 	std::cin >> N >> M >> P;
-	for (int i = 0; i < P; i++) {
-		std::cin >> power[i];
-	}
 
 	for (int i = 0; i < N; i++) {
 		std::fill(vis[i], vis[i] + M, -1);
 	}
 
+	for (int i = 0; i < P; i++) {
+		std::cin >> playerPower[i];
+	}
+
+	playerQueues.resize(P);
+
 	for (int i = 0; i < N; i++) {
 		std::cin >> board[i];
 		for (int j = 0; j < M; j++) {
 			if (board[i][j] >= '1' && board[i][j] <= '9') {
-				vis[i][j] = board[i][j] - '1';
-				castle[vis[i][j]]++;
+				int playerIdx = board[i][j] - '1';
+				if (playerIdx >= 0 && playerIdx < P) {
+					playerCastle[playerIdx]++;
+					vis[i][j] = playerIdx;
+					playerQueues[playerIdx].push({ i, j });
+				}
 			}
 		}
 	}
 }
 
-int dx[4] = { 1,0,-1,0 };
-int dy[4] = { 0,1,0,-1 };
-
-
 void Solve() {
 	bool expanded = true;
-
 	while (expanded) {
 		expanded = false;
 
-		for (int player = 0; player < P; player++) {
-			std::queue<std::pair<int, int>> currentPos;
+		for (int playerIdx = 0; playerIdx < P; playerIdx++) {
+			for (int step = 0; step < playerPower[playerIdx]; step++) {
+				int qSize = playerQueues[playerIdx].size();
+				if (qSize == 0) break; 
 
-			// 순회하면서 player 값과 같은 좌표 push
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (vis[i][j] == player) {
-						currentPos.push({ i, j });
-					}
-				}
-			}
-			// 순회하면서 player 값과 같은 좌표 push
-
-			for (int step = 0; step < power[player] && !currentPos.empty(); step++) {
-				int size = currentPos.size();
-
-				for (int k = 0; k < size; k++) {
-					auto cur = currentPos.front();
-					currentPos.pop();
+				for (int i = 0; i < qSize; i++) {
+					auto cur = playerQueues[playerIdx].front();
+					playerQueues[playerIdx].pop();
 
 					for (int dir = 0; dir < 4; dir++) {
 						int nx = cur.first + dx[dir];
@@ -67,9 +61,9 @@ void Solve() {
 						if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
 						if (board[nx][ny] == '#' || vis[nx][ny] != -1) continue;
 
-						vis[nx][ny] = player;
-						castle[player]++;
-						currentPos.push({ nx, ny });
+						vis[nx][ny] = playerIdx;
+						playerCastle[playerIdx]++;
+						playerQueues[playerIdx].push({ nx, ny });
 						expanded = true;
 					}
 				}
@@ -78,7 +72,7 @@ void Solve() {
 	}
 
 	for (int i = 0; i < P; i++) {
-		std::cout << castle[i] << " ";
+		std::cout << playerCastle[i] << " ";
 	}
 }
 
